939528f711f4cb66b6e5d49121d7ceebe12effb0
Gor777777777 <gorgoar@gmail.com>
2017-01-29 23:26:00 +0400

    Add 2 klass vertical and horizontal line

---
+++ Snake/HorizontalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        public HorizontalLine(int xRight, int xLeft,int y, char symbol)
+        {
+            horList = new List<Point>();
+            for (int x = xRight; x <= xLeft; x++)
+            {
+                Point p = new Point(x, y, symbol);
+                horList.Add(p);
+            }
+        }
+
+        List<Point> horList;
+
+        public void Drow()
+        {
+            foreach (var item in horList)
+            {
+                item.Draw();
+            }
+        }
+    }
+}
---
+++ Snake/VertiсalLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VertiсalLine
+    {
+        public VertiсalLine(int x,int yUp, int yDown, char symbol)
+        {
+            vertLine = new List<Point>();
+            for (int y =yRight 0; y < length; y++)
+            {
+
+            }
+        }
+
+        List<Point> vertLine;
+    }
+}
